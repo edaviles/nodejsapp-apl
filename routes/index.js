75023d904b40c5c229e6edf516e3d24f7095a7d2
Omar Aviles <edaviles@microsoft.com>
2022-04-07 08:47:46 -0500

    DemoTime

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '#DemoTime powered by OA' });
+  res.render('index', { title: '#DemoTime' });
 });
 
 module.exports = router;
